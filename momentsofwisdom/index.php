d4c92a7ec5c497743347b448f6b4dfd549bb0da7
Ryan <ryan.brackenbury@gmail.com>
2013-08-26 16:33:34 -0400

    I am a terribly lazy coder. Enjoy the php randomizer.

--- momentsofwisdom/index.php
+++ momentsofwisdom/index.php
@@ -12,7 +12,7 @@
       </nav>
     <?php 
  			//chooses a random number 
- 			$num = Rand (0,11) ; 
+ 			$num = Rand (0,30) ; 
  
  			//starts the random quote switch loop 
  			switch ($num) 
@@ -22,8 +22,8 @@
  				break; 
  
  				case 1: 
- 				echo "'My very first PC was a pentium 166, and we did some crazy shit and OC'd it to 200Mhz' - Beyonce Knowles, after giving birth to first child
-"; 
+ 				echo "'My very first PC was a pentium 166, and we did some crazy shit and OC'd it to 200Mhz' - 
+					Beyonce Knowles, after giving birth to first child"; 
  				break; 
  
 				case 2: 
@@ -31,7 +31,8 @@
  				break; 
  
  				case 3: 
- 				echo "'Is either the Hart House gym or the Athletic Centre open to us these days?' - Kanye West, after having a little too much to drink"; 
+ 				echo "'Is either the Hart House gym or the Athletic Centre open to us these days?' - Kanye West, 
+					after having a little too much to drink"; 
  				break; 
  
  				case 4: 
@@ -47,24 +48,108 @@
  				break; 
 
 				case 7: 
-				echo "Fead the birds."; 
+				echo "'Why not just make the kebab? Chicken isn't that expensive' - Saddam Hussein, 
+					immediately prior to being captured."; 
  				break; 
 
 				case 8: 
-				echo "Fead the birds."; 
+				echo "'Can we opt out of half the stupid shit they're charging us for?' - PM Harper, on the Kyoto Accord"; 
  				break; 
 
 				case 9: 
-				echo "Fead the birds."; 
+				echo "'LOL these r brilliant' - Obama, on the war in Syria"; 
  				break; 
 
 				case 10: 
-				echo "Fead the birds."; 
+				echo "'A penis is a her; they're like ships' - Adm. Horatio Nelsion, just prior to the Battle of Trafalgar"; 
  				break; 
 
 				case 11: 
-				echo "Fead the birds."; 
+				echo "'I am making Jerk Chicken and have mild italian sausages. Not making the Jerk chicken too hot either. 
+					Got like 6-ish legs + thighs. Figure that would be enough' - 
+					Christopher Columbus, immediately after discovering America."; 
+ 				break;
+				
+				case 12: 
+				echo "'This shit is gold' - Abraham Lincoln, during gold rush"; 
  				break; 
+
+				case 13: 
+				echo "'Why are you doing that? What is the endgame?' - Hermann Goering, 1944"; 
+ 				break; 
+
+				case 14: 
+				echo "'BUT IT IS SO GOOD IT COULD NOT STAY SILENT' - Hugh Hefner, on the subject of his penis"; 
+ 				break; 
+
+				case 15: 
+				echo "'It was in a private convo with me, wasn't it?' - Hitler, on the treaty of Versailles."; 
+ 				break; 
+
+				case 16: 
+				echo "'Linux is a kernel, it annoys me a bit when people refer to it as a complete OS 
+					because it's not.' - Microsoft CEO Steve Ballmer"; 
+ 				break; 
+
+				case 17: 
+				echo "'We figured. They're already on ebay.' - Thomas Jefferson, on slavery"; 
+ 				break; 
+
+				case 18: 
+				echo "'Homebrew beer is next up on the list after that' - Hitler, at Nuremburg Rally"; 
+ 				break; 
+
+				case 19: 
+				echo "'Yeah, earlier is better. Then I can escape.' - Kunta Kinte"; 
+ 				break; 
+
+				case 20: 
+				echo "'Isn't that illegal? Not that i'm complaining.' - Bernie Madoff"; 
+ 				break; 
+			
+				case 21: 
+				echo "'I have this sense of sexual frustration emanating from those.' - Enrico Fermi on nuclear bombs"; 
+ 				break; 
+
+				case 22: 
+				echo "'My belly hurts to laugh' -- Mister Rogers's last words"; 
+ 				break; 
+
+				case 23: 
+				echo "'We actually have to add these to some sort of archive, for personal use if nothing else. 
+					They are too good to lose.' - Vladimir Putin on feminist riots"; 
+ 				break; 
+
+				case 24: 
+				echo "'Prolly no negative repercussions to drinking hoegaarden alone while on codeine right?' - 
+					Rob Ford, shorty before cocaine allegations"; 
+ 				break; 
+
+				case 25: 
+				echo "'... works smoothly as my <REDACTED> on a Saturday morning.' - Steven Hawkings, on string theory"; 
+ 				break; 
+
+				case 26: 
+				echo "'Wheely bad joke.' - Steven Hawkings' live-in nurse."; 
+ 				break; 
+
+				case 27: 
+				echo "'Wheely bad joke.' - Steven Hawkings' live-in nurse."; 
+ 				break; 
+
+				case 28: 
+				echo "'I hear Stephen Hawking is really good at <REDACTED> Lol' - Albert Einstein, 
+					when asked about Hawking's contributions to Science"; 
+ 				break; 
+
+				case 29: 
+				echo "'I would leave the thread at this point but I know it wouldn't save me' - 
+					Lab7 C program, immediately before seg faulting"; 
+ 				break;
+
+				case 30: 
+				echo "'lol we should scrape the entire chat history...' - Edward Snowden"; 
+ 				break;  
  				} 
  		?>
   </body>
