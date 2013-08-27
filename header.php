efa5d3c68cc29d2011208bfaa5d2e1d0874fba81
Amer Hesson <amer@amerhesson.com>
2013-08-26 21:09:08 -0400

    Added header to repo.o

---
+++ header.php
@@ -0,0 +1,10 @@
+
+<header>
+    <nav>
+        <ul>
+            <li><a class="nav-item" href="http://1t6.ca">Home</a></li>
+            <li><a class="nav-item" href="http://ece.1t6.ca">ECE</a></li>
+            <li><a class="nav-item" href="http://1t6.ca/misquoted">Misquoted</a></li>
+        </ul>
+    </nav>
+</header>
