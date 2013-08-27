def4824f44c9aeb2b69473e85d819cc5754a0d0e
Ryan Brackenbury <ryan@amerhesson.com>
2013-08-26 21:06:37 -0400

    split up files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home of University of Toronto Engineering - Class of 1T6</title>
+        <link rel="stylesheet" href="/styles/main.css">
+        <script src="/js/jquery-1.10.2.js"></script>
+        <script src="/js/init.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <?php
+                $root = $_SERVER['DOCUMENT_ROOT'];
+                include_once($root . "/header.php");
+            ?>
+            <div class="content">
+                <div>Home of University of Toronto Engineering 1T6 Students.</div>
+                <div class="ryans-dream">
+                I HAVE A DREAM. SOME DAY LITTLE MIN BOYS AND LITTLE MIN GIRLS WILL BE ABLE TO SEND ELECTRONIC
+                HANDSHAKES TO VERIFY LITTLE ECE BOYS AND LITTLE ECE GIRLS AS BROTHERS AND SISTERS. 
+                1T6.CA, THE DESERT WEBSITE, SWELTERING IN THE HEAT OF WIFI SIGNALS AND VIRAL ATTACKS, 
+                WILL BECOME AND OASIS OF DIGITAL FREEDOM AND JUSTICE. 
+                </div>
+            </div>
+            <?php
+                include_once($root . "/footer.php");
+            ?>
+        </div>
+    </body>
+</html>
