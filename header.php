5b75266292b13a3283aa27f0a0a2c2956653eb75
Ryan B <ryan.brackenbury@gmail.com>
2013-08-26 23:25:30 -0400

    Added IRC link

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
-
 <header>
     <nav>
         <ul>
             <li><a class="nav-item" href="http://1t6.ca">Home</a></li>
             <li><a class="nav-item" href="http://ece.1t6.ca">ECE</a></li>
+            <li><a class="nav-item" href="http://irc.1t6.ca">IRC</a></li>
             <li><a class="nav-item" href="http://1t6.ca/misquoted">Misquoted</a></li>
         </ul>
     </nav>
