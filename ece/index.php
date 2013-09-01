93e7f48ad64486388ecb51ae468c366e26470e91
Ryan B <ryan.brackenbury@gmail.com>
2013-08-31 23:27:00 -0400

    Added 404'd.

--- ece/index.php
+++ ece/index.php
@@ -3,5 +3,17 @@
     <head>
     </head>
     <body>
+        <div class="content">
+                <div class="irc-info">
+                  404'd!
+                  
+                  <iframe width="640"
+                  height="360" 
+                  src="http://www.youtube.com/embed/oHg5SJYRHA0?feature=player_detailpage" 
+                  frameborder="0" 
+                  allowfullscreen>
+                  </iframe>
+                </div>
+        <div>
     </body>
 </html>
